3edace0bff7796a293ceab63c5b031c9794a339f
PaulVincent0127 <paultawas48@gmail.com>
2023-09-02 15:52:14 +0800

    second

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-       function fetchAndQueryJSON() {
-            const url = document.getElementById('urlInput').value;
+function fetchAndQueryJSON() {
+    const url = document.getElementById('urlInput').value;
 
-            async function fetchAndDisplayJSON() {
-                try {
-                    const response = await fetch(url);
-                    const data = await response.json();
+    async function fetchAndDisplayJSON() {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
 
-                    // Convert the JSON array to a string for display
-                    const jsonArrayString = JSON.stringify(data, null, 2);
+            // Convert the JSON array to a string for display
+            const jsonArrayString = JSON.stringify(data, null, 2);
 
-                    // Display the JSON array on the page
-                    document.getElementById('filteredJson').textContent = jsonArrayString;
-                } catch (error) {
-                    console.error('Error fetching data:', error);
-                    document.getElementById('filteredJson').textContent = 'Error fetching data.';
-                }
-            }
+            // Display the JSON array on the page
+           document.getElementById('filteredJson').textContent = jsonArrayString;
 
-            // Call the function to fetch and display the JSON array
-            fetchAndDisplayJSON();
-        }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            document.getElementById('filteredJson').textContent = 'Error fetching data.';
+        }       
+    }
 
-        
+
+
+    // Call the function to fetch and display the JSON array
+    fetchAndDisplayJSON();
+}
+
+
+
