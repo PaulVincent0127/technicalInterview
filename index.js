f7d471a0652e2550eaeeab8c2c1ed08b99bf2c39
PaulVincent0127 <paultawas48@gmail.com>
2023-09-02 17:02:53 +0800

    third

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-function fetchAndQueryJSON() {
-    const url = document.getElementById('urlInput').value;
+// Function to sort characters in a string in descending order
+function sortStringDescending(str) {
+    return str.split('').sort((a, b) => b.localeCompare(a)).join('');
+  }
 
-    async function fetchAndDisplayJSON() {
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
+  // Function to recursively sort an object's keys and values
+  function sortObjectDescending(obj) {
+    const sortedObj = {};
 
-            // Convert the JSON array to a string for display
-            const jsonArrayString = JSON.stringify(data, null, 2);
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        const sortedKey = sortStringDescending(key);
+        const value = obj[key];
 
-            // Display the JSON array on the page
-           document.getElementById('filteredJson').textContent = jsonArrayString;
-
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            document.getElementById('filteredJson').textContent = 'Error fetching data.';
-        }       
+        if (typeof value === 'object' && value !== null) {
+          // If the value is an object, recursively sort it
+          sortedObj[sortedKey] = sortObjectDescending(value);
+        } else if (typeof value === 'string') {
+          // If the value is a string, sort it
+          sortedObj[sortedKey] = sortStringDescending(value);
+        } else {
+          // Otherwise, keep the value as is
+          sortedObj[sortedKey] = value;
+        }
+      }
     }
 
+    return sortedObj;
+  }
 
+  // Event listener for the button click
+  document.getElementById('descButton').addEventListener('click', function () {
+    const url = document.getElementById('urlInput').value;
 
-    // Call the function to fetch and display the JSON array
-    fetchAndDisplayJSON();
-}
+    // Fetch the JSON data from the user-entered URL
+    fetch(url)
+      .then(response => response.json())
+      .then(jsonData => {
+        // Call the sorting function on the JSON object
+        const sortedJsonData = sortObjectDescending(jsonData);
 
-
-
+        // Display the sorted JSON data
+        document.getElementById('descJson').textContent = JSON.stringify(sortedJsonData, null, 2);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        document.getElementById('descJson').textContent = 'Error fetching data.';
+      });
+  });
